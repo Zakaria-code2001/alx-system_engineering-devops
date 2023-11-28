51099462f401a1836a8975e0cdcbc3705b999cb1
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2023-11-28 05:22:58 -0800

    task1

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
-#Find the regular expression that will match the shown cases
-
-puts ARGV[0].scan(/hbt{2,5}n/).join
+# A Ruby script that accepts one argument and pass it to a 
+# regular expression matching method
+#
+puts ARGV[0].scan(/(h)(b)(t{2,5})(n)/).join
