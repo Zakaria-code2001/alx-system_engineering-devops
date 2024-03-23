6f8c30b53b0b115431c41fa03c520546f4b67e67
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-03-22 19:48:39 -0700

    f

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,4 +2,10 @@
 package { 'puppet-lint':
   ensure   => '2.1.1',
   provider => 'gem',
+} 
+# Installs the package Werkzeug
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['puppet-lint'],
 }
