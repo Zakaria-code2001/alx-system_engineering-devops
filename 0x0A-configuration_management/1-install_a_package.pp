046a51307d6a4ce7c87595aac00ea5c859d671f9
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-03-22 19:52:41 -0700

    ff

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,4 +3,8 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
-
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['flask'], # Ensure Flask is installed first
+}
