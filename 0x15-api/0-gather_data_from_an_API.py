be4dbafbb4919e23ad621baa770acf7d83648f32
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-04-29 10:16:44 +0200

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-'''
+"""
 gather employee data from API
-'''
+"""
 
 import re
 import requests
 import sys
 
+
 def fetch_employee_todo_list(employee_id):
     # API URL to fetch employee TODO list information
-    url = f"https://jsonplaceholder.typicode.com/todos/{employee_id}"
+    url = (f"https://jsonplaceholder.typicode.com/todos/"
+           f"{employee_id}")
 
     # Fetch data from the API
     response = requests.get(url)
@@ -19,14 +21,18 @@ def fetch_employee_todo_list(employee_id):
         data = response.json()
         employee_name = data.get('name', 'Unknown')
         total_tasks = len(data)
-        completed_tasks = [task for task in data if task['completed']]
+        completed_tasks = [task for task in data if
+                           task['completed']]
         num_completed_tasks = len(completed_tasks)
 
         # Display information
-        print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
+        print(f"Employee {employee_name} is done with"
+              f" tasks ({num_completed_tasks}/{total_tasks}):")
         for task in completed_tasks:
             print(f"\t{task['title']}")
     else:
-        print(f"Failed to fetch TODO list for employee {employee_id}")
+        print(f"Failed to fetch TODO list"
+              f" for employee {employee_id}")
+
 
 fetch_employee_todo_list(1)
