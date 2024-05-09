f8c44c8c6d2113becf7e1041ac21e0bb67db8f8b
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-05-09 14:03:53 +0200

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""Module for task 1 advanced """
+import requests
+import sys
+
+
+def top_ten(subreddit):
+    """
+    function that queries the Reddit API and
+    returns the number of subscribers
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            title = post.get('data').get('title')
+            print(title)
+    elif (not response.ok):
+        return 0
