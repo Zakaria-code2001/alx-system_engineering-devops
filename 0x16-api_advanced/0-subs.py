1b59faf1b26d3ca676bddec1ce201de199beef56
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-05-09 10:00:48 +0200

    task 0  api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,8 @@ import sys
 
 def number_of_subscribers(subreddit):
     """
-    function that queries the Reddit API and
-    returns the number of subscribers
+    Function that queries the Reddit API and returns the number of subscribers.
+    If the subreddit is not valid, returns 0.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
