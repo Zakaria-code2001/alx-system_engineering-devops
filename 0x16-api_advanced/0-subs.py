82e1d0bd2ae3757f9a0e9617883d1b24c1aa05e9
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-05-09 10:11:40 +0200

    update on task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for task 0 advanced """
 import requests
+from requests import get
 import sys
 
 
@@ -9,16 +10,16 @@ def number_of_subscribers(subreddit):
     function that queries the Reddit API and
     returns the number of subscribers
     """
-    
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers_count = data['data']['subscribers']
-        return subscribers_count
-    elif (not response.ok):
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
-    else:
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
         return 0
